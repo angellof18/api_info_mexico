79b2465a407815dc3702096214cdd9ff9e5b08c4
angellof18 <arteaga.angel.1720@hotmail.com>
2024-10-07 21:28:58 -0600

    Nuevo endpoint

---
+++ src/routes/api/estados/[id]/municipios/[municipioId]/+server.js
@@ -0,0 +1,35 @@
+import estados from '$lib/data.json';
+
+export function GET({ params }) {
+	const { id, municipioId } = params;
+
+	const estadoData = estados.find((e) => e.EFE_KEY === parseInt(id));
+
+	if (!estadoData) {
+		return new Response('Estado no encontrado', { status: 404 });
+	}
+
+	const municipios = estadoData.MUNICIPIOS;
+
+	if (municipios.length === 0) {
+		return new Response('No hay municipios disponibles para este estado', { status: 404 });
+	}
+
+	const municipio = municipios.find((m) => m.MUN_KEY === parseInt(municipioId));
+
+	if (!municipio) {
+		return new Response('Municipio no encontrado', { status: 404 });
+	}
+
+	const responseData = {
+		id: municipio.MUN_KEY,
+		nombre: municipio.MUNICIPIO
+	};
+
+	return new Response(JSON.stringify(responseData), {
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*'
+		}
+	});
+}
